6efd675d616e90b8eb3377ad16aaa31bd3a0477d
Michael Cadiz <mike@cadizm.com>
2023-11-11 15:16:52 -0800

    Setup Bazel

---
+++ src/main/java/com/cadizm/runner/Runner.java
@@ -0,0 +1,11 @@
+package com.cadizm.runner;
+
+import com.cadizm.algorithms.DepthFirstSearch;
+
+public class Runner {
+
+  public static void main(String args[]) {
+    System.out.println("Main runner");
+    DepthFirstSearch.dfs();
+  }
+}
