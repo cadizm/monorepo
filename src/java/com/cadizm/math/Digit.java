d719315283f8ece2a38f7911dfc47316b7c2ef15
Michael Cadiz <mike@cadizm.com>
2023-11-24 11:18:50 -0800

    Add math package and deleteSingleDigit

---
+++ src/java/com/cadizm/math/Digit.java
@@ -0,0 +1,23 @@
+package com.cadizm.math;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Digit {
+
+  /**
+   * Return the set of numbers resulting from the deletion of a single digit
+   * from positive number n (i.e. greater than 0).
+   */
+  static Set<Integer> deleteSingleDigit(int n) {
+
+    Set<Integer> res = new HashSet<>();
+
+    for (int d = 1; d <= n; d *= 10) {
+      int number = (n % d) + (((n / d) / 10) * d);
+      res.add(number);
+    }
+
+    return res;
+  }
+}
