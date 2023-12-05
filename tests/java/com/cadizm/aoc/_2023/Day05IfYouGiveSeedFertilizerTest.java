465548ead3c2b0279f297152aae7fdc248a309a8
Michael Cadiz <mike@cadizm.com>
2023-12-05 09:08:00 -0800

    [WIP] Add day 5 parts 1 and 2
    Note: part 2 passes, but is unoptimized, still WIP

---
+++ tests/java/com/cadizm/aoc/_2023/Day05IfYouGiveSeedFertilizerTest.java
@@ -0,0 +1,19 @@
+package com.cadizm.aoc._2023;
+
+import org.junit.jupiter.api.Test;
+
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Day05IfYouGiveSeedFertilizerTest {
+
+  @Test
+  public void testPuzzle1() {
+    //assertEquals(21138, new Day04Scratchcards().puzzle1());
+    System.out.println(new Day05IfYouGiveSeedFertilizer().puzzle1());
+  }
+
+  @Test
+  public void testPuzzle2() {
+    System.out.println(new Day05IfYouGiveSeedFertilizer().puzzle2());
+  }
+}
